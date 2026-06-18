4357101960e7c6f9a3a3d4b104313868a2c38be4
Vishal Singh <vishal017017@gmail.com>
2026-06-18 18:20:44 +0530

    Fix Render logging

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ explainer = joblib.load(
 # ==========================================
 
 logging.basicConfig(
-    filename="logs/app.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
